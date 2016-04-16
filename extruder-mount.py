5d689341328acd4e32bd8ad6035ae6006e5783e5
David Bebb <davidbebb@gmail.com>
2016-04-16 22:29:55 +0100

    added extruder block and cover/mount

--- extruder-mount.py
+++ extruder-mount.py
@@ -8,10 +8,16 @@ spacer_cutter       = 'laser'
 spacer_material     = 'perspex'
 spacer_thickness    = 3
 
-motor = 'nema1.7'
+cover_cutter = 'laser'
+cover_material = 'perspex'
+cover_thickness = 3
+
+motor = 'NEMA1.7'
 
 width =  42.3
 height = 13.7
+
+corner_rad = 3
 centre = V(0,0)
 stepper_pos = V(0, (42.3/2)-(height/2))
 
@@ -31,11 +37,24 @@ plane.add_layer(
     # cutter = extruder_cutter,
     z0 = 0)
 
+plane.add_layer(
+    'cover',
+    material = cover_material,
+    thickness =cover_thickness,
+    z0 = 0)
+
 border = RoundedRect(
     centre,
     width = width,
     height = height,
-    rad = 1,
+    rad = corner_rad,
+    centred = True)
+
+cover_border = RoundedRect(
+    stepper_pos + V(0, height/2),
+    width = width,
+    height = width+height,
+    rad = corner_rad,
     centred = True)
 
 spacer = plane.add_path(
@@ -46,7 +65,8 @@ spacer = plane.add_path(
     )
 )
 
-stepper = spacer.add(Stepper(stepper_pos, 'NEMA1.7', 'spacer'))
+
+stepper = spacer.add(Stepper(stepper_pos, motor, 'spacer'))
 
 extruder = plane.add_path(
     Part(name = 'extrued_part',
@@ -56,9 +76,25 @@ extruder = plane.add_path(
     )
 )
 
-print stepper.d
+d = stepper.d
+print d
+extruder.add(Hole(stepper_pos+V(d['bolt_sep']/2, -d['bolt_sep']/2), rad=milling.bolts[d['bolt_size']]['clearance']/2))
+extruder.add(Hole(stepper_pos+V(-d['bolt_sep']/2, -d['bolt_sep']/2), rad=milling.bolts[d['bolt_size']]['clearance']/2))
 
-# d = pStepper['NEMA1.7']
-#
-# spacer.add(Hole(stepper_pos+V(d['bolt_sep']/2,d['bolt_sep']/2), rad=milling.bolts[d['bolt_size']]['clearance']/2),layer)
-# spacer.add(Hole(stepper_pos+V(-d['bolt_sep']/2,d['bolt_sep']/2), rad=milling.bolts[d['bolt_size']]['clearance']/2))
+cover = plane.add_path(
+    Part(name = 'cover_part',
+        border = cover_border,
+        layer = 'cover',
+        cutter = cover_cutter
+    )
+)
+
+
+cover.add(Hole(stepper_pos+V(d['bolt_sep']/2, -d['bolt_sep']/2), rad=milling.bolts[d['bolt_size']]['clearance']/2))
+cover.add(Hole(stepper_pos+V(-d['bolt_sep']/2, -d['bolt_sep']/2), rad=milling.bolts[d['bolt_size']]['clearance']/2))
+cover.add(Hole(stepper_pos+V(d['bolt_sep']/2, d['bolt_sep']/2), rad=milling.bolts[d['bolt_size']]['clearance']/2))
+cover.add(Hole(stepper_pos+V(-d['bolt_sep']/2, d['bolt_sep']/2), rad=milling.bolts[d['bolt_size']]['clearance']/2))
+cover.add(Hole(stepper_pos, rad=d['shaft_diam']/2+1))
+
+cover.add(Hole(stepper_pos+V(d['bolt_sep']/2, d['bolt_sep']/1.05), rad=milling.bolts[d['bolt_size']]['clearance']/2))
+cover.add(Hole(stepper_pos+V(-d['bolt_sep']/2, d['bolt_sep']/1.05), rad=milling.bolts[d['bolt_size']]['clearance']/2))
